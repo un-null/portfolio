d03bc82b7b93479afb87e6443e56387d0242f13e
un-null <movigam.0902@gmail.com>
2022-11-07 21:18:29 +0900

    ♻️

--- src/component/Layout/Header.tsx
+++ src/component/Layout/Header.tsx
@@ -1,14 +1,14 @@
 import { ActionIcon, Anchor, useMantineColorScheme } from '@mantine/core'
 import Link from 'next/link'
 import { FC } from 'react'
-import { IconMoon, IconSun, IconPlanet } from '@tabler/icons'
+import { IconMoon, IconPlanet, IconSunLow } from '@tabler/icons'
 
 export const Header: FC = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme()
   const dark = colorScheme === 'dark'
 
   return (
-    <header className="sticky top-0 z-50 grid h-14 w-full place-items-center bg-[#1A1B1E] px-6">
+    <header className="sticky top-0 z-50 grid h-14 w-full place-items-center bg-transparent px-6">
       <div className="flex h-full w-full max-w-screen-lg items-center justify-between">
         <Link href="/" passHref>
           <Anchor
@@ -16,7 +16,7 @@ export const Header: FC = () => {
             size="xl"
             weight={700}
             underline={false}
-            color="dark.0"
+            color={dark ? 'dark.0' : 'dark.7'}
             className="flex"
           >
             <span className="mr-2">
@@ -27,24 +27,45 @@ export const Header: FC = () => {
         </Link>
         <div className="flex space-x-6">
           <nav>
-            <Link href="/" passHref>
-              <Anchor
-                component="a"
-                size="lg"
-                weight={700}
-                underline={false}
-                color="dark.7"
-              >
-                Blog
-              </Anchor>
-            </Link>
+            <ul className="flex space-x-2">
+              <li>
+                <Link href="/" passHref>
+                  <Anchor
+                    component="a"
+                    size="md"
+                    weight={500}
+                    underline={false}
+                    color={dark ? 'dark.0' : 'dark.7'}
+                  >
+                    Blog
+                  </Anchor>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact" passHref>
+                  <Anchor
+                    component="a"
+                    size="md"
+                    weight={500}
+                    underline={false}
+                    color={dark ? 'dark.0' : 'dark.7'}
+                  >
+                    contact
+                  </Anchor>
+                </Link>
+              </li>
+            </ul>
           </nav>
           <ActionIcon
             variant="outline"
-            color={dark ? 'yellow' : 'blue'}
+            className={`${
+              dark
+                ? 'border-[#F4E99B] text-[#F4E99B]'
+                : 'border-[#506886] text-[#506886]'
+            }`}
             onClick={() => toggleColorScheme()}
           >
-            {dark ? <IconSun size={20} /> : <IconMoon size={20} />}
+            {dark ? <IconSunLow size={20} /> : <IconMoon size={20} />}
           </ActionIcon>
         </div>
       </div>
